a195aa1235ba798721118af40e3c15285b7f6b6b
AndrewKW <accumhelp@ya.ru>
2024-01-29 13:12:13 +0300

    0

--- ConsoleQuestISP2k/Program.cs
+++ ConsoleQuestISP2k/Program.cs
@@ -1,9 +1,61 @@
 ﻿internal class Program
 {
+    static char nextScene;
+    private static void FirstScene()
+    {
+        //вывод на экран варианта сцены.
+        Console.WriteLine("Вы подошили к воротам техникума...\nВаши действия?");
+        Console.WriteLine("1. Пойти на пары");
+        Console.WriteLine("2. Уйти домой");
+        Console.WriteLine("3. Пойти за шавой");
+        string userChoose = Console.ReadLine();
+        Console.WriteLine("Выбор " + userChoose[0]);
+        nextScene = userChoose[0];
+    }
+
+    private static char Scene0()
+    {
+        //вывод на экран варианта сцены.
+        Console.WriteLine("Вы подошили к воротам техникума...\nВаши действия?");
+        Console.WriteLine("1. Пойти на пары");
+        Console.WriteLine("2. Уйти домой");
+        Console.WriteLine("3. Пойти за шавой");
+        string userChoose = Console.ReadLine();
+        Console.WriteLine("Выбор " + userChoose[0]);
+        
+        switch (userChoose[0])
+        {
+            case '1':
+                return '1';
+                break;
+            case '2':
+                return '2';
+                break;
+            case '3':
+                return '3';
+                break;
+            default:
+                Scene0();
+                return '*';
+                break;
+
+        }        
+       
+    }
+
+    //сцена ушёл на пары.
+    private static void Scene1()
+    {
+        Console.WriteLine("Вы пришли в аудиторию \nполную Ваших друзей. \nЗвинит звонок.");
+    }
+    private static void Scene2()
+    {
+        Console.WriteLine("Вы ушли домой");
+    }
     private static void Main(string[] args)
     {
         string myString;
-        int helthPower = 100; 
+        int helthPower = 100;
         int carmaVal = 0;
         string palayerName = "noname";
         /*
@@ -15,15 +67,17 @@
         //инкапсуляция текстовой переменной.
         Console.WriteLine($"Ваше здоровье: {helthPower}, карма {carmaVal}");
 
+        Scene0();
+
         //вывод на экран варианта сцены.
-        Console.WriteLine("Вы подошили к воротам техникума...\nВаши действия?");
+        /*Console.WriteLine("Вы подошили к воротам техникума...\nВаши действия?");
         Console.WriteLine("1. Пойти на пары");
         Console.WriteLine("2. Уйти домой");
         Console.WriteLine("3. Пойти за шавой");
         Console.Beep();
         string userChoose = Console.ReadLine();
         Console.WriteLine("Выбор " + userChoose[0]);
-        
+        */
         //Использование конструкции if then else.
         /*
         if (userChoose[0] == '1') 
@@ -43,6 +97,7 @@
             Console.WriteLine("Такого варианта нет, повторите ввод");
         }
         */
+        /*
         switch (userChoose[0])
         {
             case '1':
@@ -57,6 +112,24 @@
             default:
                 Console.WriteLine("Такого варианта нет, повторите ввод");
                 break;
-        }
+        }*/
+
+        //FirstScene();
+
+        //switch (Scene0())
+        //{
+        //    case '1':
+        //        Scene1();
+        //        break;
+        //    case '2':
+        //        Scene2();
+        //        break;
+        //    case '3':
+        //        break;
+        //    default:
+        //        Console.WriteLine("Такого варианта нет, повторите ввод");
+        //        FirstScene();
+        //        break;
+        //}
     }
 }
